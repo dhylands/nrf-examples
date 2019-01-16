b4ae560e7624a48b1ffe02c08a905e8064f6da25
Dave Hylands <dhylands@gmail.com>
2019-01-16 10:10:36 -0800

    Started to add second CDC ACM port

--- zigbee/cli_agent_router/main.c
+++ zigbee/cli_agent_router/main.c
@@ -52,19 +52,61 @@
 
 #include "nrf_drv_clock.h"
 #include "boards.h"
+#include "bsp.h"
 #include "app_timer.h"
+#include "app_error.h"
+#include "app_util.h"
+#include "app_usbd_core.h"
+#include "app_usbd.h"
+#include "app_usbd_string_desc.h"
+#include "app_usbd_cdc_acm.h"
+#include "app_usbd_serial_num.h"
+
 
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
+
 #define IEEE_CHANNEL_MASK                   (1l << ZIGBEE_CHANNEL)              /**< Scan only one, predefined channel to find the coordinator. */
-#define ERASE_PERSISTENT_CONFIG             ZB_TRUE                             /**< Do not erase NVRAM to save the network parameters after device reboot or power-off. NOTE: If this option is set to ZB_TRUE then do full device erase for all network devices before running other samples. */
-#define ZIGBEE_NETWORK_STATE_LED            BSP_BOARD_LED_2                     /**< LED indicating that light switch successfully joind ZigBee network. */
+#define ERASE_PERSISTENT_CONFIG             ZB_FALSE                            /**< Do not erase NVRAM to save the network parameters after device reboot or power-off. NOTE: If this option is set to ZB_TRUE then do full device erase for all network devices before running other samples. */
+
+#define LED_USB_RESUME              (BSP_BOARD_LED_0)
+#define LED_CDC_ACM_OPEN            (BSP_BOARD_LED_1)
+#define ZIGBEE_NETWORK_STATE_LED    (BSP_BOARD_LED_2)   /**< LED indicating that light switch successfully joind ZigBee network. */
+
+#define BTN_CDC_DATA_SEND       0
+#define BTN_CDC_NOTIFY_SEND     1
+#define BTN_CDC_DATA_KEY_RELEASE        (bsp_event_t)(BSP_EVENT_KEY_LAST + 1)
 
 #if !defined ZB_ROUTER_ROLE
 #error Define ZB_ROUTER_ROLE to compile CLI agent (Router) source code.
 #endif
+
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event);
+
+#define CDC_ACM_COMM_INTERFACE  0
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN2
+
+#define CDC_ACM_DATA_INTERFACE  1
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN1
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT1
+
+#define READ_SIZE 1
+
+/**
+ * @brief CDC_ACM class instance
+ * */
+APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
+                            cdc_acm_user_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250
+);
 
 typedef struct cli_agent_ctx_s
 {
@@ -76,6 +118,11 @@ static zb_uint8_t         m_attr_zcl_version   = ZB_ZCL_VERSION;
 static zb_uint8_t         m_attr_power_source  = ZB_ZCL_BASIC_POWER_SOURCE_UNKNOWN;
 static zb_uint16_t        m_attr_identify_time = 0;
 static zb_bool_t          m_stack_started      = ZB_FALSE;
+static char               m_rx_buffer[READ_SIZE];
+static char               m_tx_buffer[NRF_DRV_USBD_EPSIZE];
+static bool               m_send_flag = 0;
+
+
 
 /* Declare attribute list for Basic cluster. */
 ZB_ZCL_DECLARE_BASIC_ATTRIB_LIST(basic_attr_list, &m_attr_zcl_version, &m_attr_power_source);
@@ -96,6 +143,142 @@ ZB_HA_DECLARE_CONFIGURATION_TOOL_EP(cli_agent_ep,
 
 /* Declare application's device context (list of registered endpoints) for CLI Agent device. */
 ZB_HA_DECLARE_CONFIGURATION_TOOL_CTX(cli_agent_ctx, cli_agent_ep);
+
+/**
+ * @brief User event handler @ref app_usbd_cdc_acm_user_ev_handler_t (headphones)
+ * */
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event)
+{
+    app_usbd_cdc_acm_t const * p_cdc_acm = app_usbd_cdc_acm_class_get(p_inst);
+
+    switch (event)
+    {
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+        {
+            bsp_board_led_on(LED_CDC_ACM_OPEN);
+
+            /*Setup first transfer*/
+            ret_code_t ret = app_usbd_cdc_acm_read(p_cdc_acm,
+                                                   m_rx_buffer,
+                                                   READ_SIZE);
+            UNUSED_VARIABLE(ret);
+            break;
+        }
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+            bsp_board_led_off(LED_CDC_ACM_OPEN);
+            break;
+        case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+            //bsp_board_led_invert(LED_CDC_ACM_TX);
+            break;
+        case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+        {
+            ret_code_t ret;
+            NRF_LOG_INFO("Bytes waiting: %d",
+                         app_usbd_cdc_acm_bytes_stored(p_cdc_acm));
+            do
+            {
+                /*Get amount of data transfered*/
+                size_t size = app_usbd_cdc_acm_rx_size(p_cdc_acm);
+                NRF_LOG_INFO("RX: size: %lu char: %c",
+                             size, m_rx_buffer[0]);
+
+                /* Fetch data until internal buffer is empty */
+                ret = app_usbd_cdc_acm_read(p_cdc_acm,
+                                            m_rx_buffer,
+                                            READ_SIZE);
+            } while (ret == NRF_SUCCESS);
+
+            //bsp_board_led_invert(LED_CDC_ACM_RX);
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+static void usbd_user_ev_handler(app_usbd_event_type_t event)
+{
+    switch (event)
+    {
+        case APP_USBD_EVT_DRV_SUSPEND:
+            bsp_board_led_off(LED_USB_RESUME);
+            break;
+        case APP_USBD_EVT_DRV_RESUME:
+            bsp_board_led_on(LED_USB_RESUME);
+            break;
+        case APP_USBD_EVT_STARTED:
+            break;
+        case APP_USBD_EVT_STOPPED:
+            app_usbd_disable();
+            bsp_board_leds_off();
+            break;
+        case APP_USBD_EVT_POWER_DETECTED:
+            NRF_LOG_INFO("USB power detected");
+
+            if (!nrf_drv_usbd_is_enabled())
+            {
+                app_usbd_enable();
+            }
+            break;
+        case APP_USBD_EVT_POWER_REMOVED:
+            NRF_LOG_INFO("USB power removed");
+            app_usbd_stop();
+            break;
+        case APP_USBD_EVT_POWER_READY:
+            NRF_LOG_INFO("USB ready");
+            app_usbd_start();
+            break;
+        default:
+            break;
+    }
+}
+
+static void bsp_event_callback(bsp_event_t ev)
+{
+    ret_code_t ret;
+    switch ((unsigned int)ev)
+    {
+        case CONCAT_2(BSP_EVENT_KEY_, BTN_CDC_DATA_SEND):
+        {
+            m_send_flag = 1;
+            break;
+        }
+
+        case BTN_CDC_DATA_KEY_RELEASE :
+        {
+            m_send_flag = 0;
+            break;
+        }
+
+        case CONCAT_2(BSP_EVENT_KEY_, BTN_CDC_NOTIFY_SEND):
+        {
+            ret = app_usbd_cdc_acm_serial_state_notify(&m_app_cdc_acm,
+                                                       APP_USBD_CDC_ACM_SERIAL_STATE_BREAK,
+                                                       false);
+            UNUSED_VARIABLE(ret);
+            break;
+        }
+
+        default:
+            return; // no implementation needed
+    }
+}
+
+static void init_bsp(void)
+{
+    ret_code_t ret;
+    ret = bsp_init(BSP_INIT_BUTTONS, bsp_event_callback);
+    APP_ERROR_CHECK(ret);
+
+    UNUSED_RETURN_VALUE(bsp_event_to_button_action_assign(BTN_CDC_DATA_SEND,
+                                                          BSP_BUTTON_ACTION_RELEASE,
+                                                          BTN_CDC_DATA_KEY_RELEASE));
+
+    /* Configure LEDs */
+    bsp_board_init(BSP_INIT_LEDS);
+    bsp_board_leds_off();
+}
 
 static void log_init(void)
 {
@@ -186,12 +369,13 @@ int main(void)
 {
     ret_code_t     ret;
     zb_ieee_addr_t ieee_addr;
+    static const app_usbd_config_t usbd_config = {
+        .ev_state_proc = usbd_user_ev_handler
+    };
 
     UNUSED_VARIABLE(m_device_ctx);
 
-    /* Intiialise the leds */
-    bsp_board_init(BSP_INIT_LEDS);
-    bsp_board_leds_off();
+    init_bsp();
 
     /* Initialize loging system and GPIOs. */
     log_init();
@@ -205,8 +389,21 @@ int main(void)
     ret = app_timer_init();
     APP_ERROR_CHECK(ret);
 
+    app_usbd_serial_num_generate();
+
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+    NRF_LOG_INFO("USBD CDC ACM example started.");
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&m_app_cdc_acm);
+    ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+
     /* Initialize the Zigbee CLI subsystem */
     zb_cli_init(ZIGBEE_CLI_ENDPOINT);
+
+    ret = app_usbd_power_events_enable();
+    APP_ERROR_CHECK(ret);
 
     /* Set ZigBee stack logging level and traffic dump subsystem. */
     ZB_SET_TRACE_LEVEL(ZIGBEE_TRACE_LEVEL);
@@ -232,12 +429,27 @@ int main(void)
     /* Start ZigBee stack. */
     while(1)
     {
+        if(m_send_flag)
+        {
+            static int  frame_counter;
+
+            size_t size = sprintf(m_tx_buffer, "Hello USB CDC FA demo: %u\r\n", frame_counter);
+
+            ret = app_usbd_cdc_acm_write(&m_app_cdc_acm, m_tx_buffer, size);
+            if (ret == NRF_SUCCESS)
+            {
+                ++frame_counter;
+            }
+        }
         if (m_stack_started)
         {
             zboss_main_loop_iteration();
         }
         UNUSED_RETURN_VALUE(NRF_LOG_PROCESS());
         UNUSED_RETURN_VALUE(zb_cli_process());
+
+        /* Sleep CPU only if there was no interrupt since last loop processing */
+        __WFE();
     }
 }
 
